9fc228c4e455ca69af594b88d0798d91ac1c314a
Ignat Zakrevsky <iezakrevsky@gmail.com>
2017-09-15 18:16:22 +0300

    Fetch sha commit if not passed

--- lib/adapters/custom.rb
+++ lib/adapters/custom.rb
@@ -11,7 +11,13 @@ module Adapters
     end
 
     def sha
-      @sha ||= options.fetch(:sha)
+      @sha ||= options.fetch(:sha) { octokit.pull_request(repo, id).head.sha }
+    end
+
+    private
+
+    def octokit
+      @octokit ||= Octokit::Client.new(access_token: ENV.fetch('GITHUB_TOKEN'))
     end
   end
 end
