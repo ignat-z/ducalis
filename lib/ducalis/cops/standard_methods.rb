f9147bd7e27e7e01f2fcdbeae6ae8c94bf933715
Ignat Zakrevsky <iezakrevsky@gmail.com>
2017-12-02 21:56:16 +0300

    [#9] Add cop for redefine default ruby methods (#48)

---
+++ lib/ducalis/cops/standard_methods.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rubocop'
+
+module Ducalis
+  class StandardMethods < RuboCop::Cop::Cop
+    BLACK_LIST = [Object].flat_map { |klass| klass.new.methods }
+
+    OFFENSE = <<-MESSAGE.gsub(/^ +\|\s/, '').strip
+      | Please, be sure that you really want to redefine standard ruby methods.
+      | You should know what are you doing and all consequences.
+    MESSAGE
+
+    def on_def(node)
+      name, _args, _body = *node
+      return unless BLACK_LIST.include?(name)
+      add_offense(node, :expression, OFFENSE)
+    end
+  end
+end
---
+++ spec/ducalis/cops/standard_methods_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require './lib/ducalis/cops/standard_methods'
+
+RSpec.describe Ducalis::StandardMethods do
+  subject(:cop) { described_class.new }
+
+  it 'raises if use redefines default ruby methods' do
+    inspect_source(cop, [
+                     'def to_s',
+                     '  "my version"',
+                     'end'
+                   ])
+    expect(cop).to raise_violation(/redefine standard/)
+  end
+
+  it 'ignores if use defines simple ruby methods' do
+    inspect_source(cop, [
+                     'def present',
+                     '  "my version"',
+                     'end'
+                   ])
+    expect(cop).to_not raise_violation
+  end
+end
