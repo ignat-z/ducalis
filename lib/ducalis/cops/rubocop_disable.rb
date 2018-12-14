bc95439f7110cf7517c99333c6d05d0715935c6e
Ignat Zakrevsky <iezakrevsky@gmail.com>
2018-12-14 18:16:55 +0100

    [#99] Add comments for most popular rubocop disablings
    It seems like I will need to move all this messages into a config.
    May be I will make it when I will remove rubocop as a dependency.

--- lib/ducalis/cops/rubocop_disable.rb
+++ lib/ducalis/cops/rubocop_disable.rb
@@ -8,14 +8,58 @@ module Ducalis
       | Please, do not suppress RuboCop metrics, may be you can introduce some refactoring or another concept.
     MESSAGE
 
+    MAGIC_SUBSTRING = 'rubocop:disable'.freeze
+
+    ABC_SIZE = <<-MESSAGE.gsub(/^ +\|\s/, '').strip
+      | This method seems to be too complex, try to split it into smaller ones. You can use merging for complex hash building.
+    MESSAGE
+
+    METHOD_LENGTH = <<-MESSAGE.gsub(/^ +\|\s/, '').strip
+      | It seems like this method contains too much logic. Please, split it up into several methods, sometimes even separate classes can be a good idea.
+    MESSAGE
+
+    CLASS_LENGTH = <<-MESSAGE.gsub(/^ +\|\s/, '').strip
+      | This class became too big. Please, try to extract a functionality into a smaller classes. If it's a model, try to extract method into classes.
+    MESSAGE
+
+    COP_PATTERN = %r{
+      (?<copname>       # Named group
+        [A-Za-z]+       # Module
+        \/              # Separator
+        [A-Za-z]+       # Class
+      )[ \n]*           # Newline or space
+    }x.freeze # ex: Metrics/ParameterLists
+
+    SUGGESTIONS = {
+      'Metrics/AbcSize' => ABC_SIZE,
+      'Metrics/ClassLength' => CLASS_LENGTH,
+      'Metrics/MethodLength' => METHOD_LENGTH
+    }.freeze
+
     def investigate(processed_source)
       return unless processed_source.ast
 
       processed_source.comments.each do |comment_node|
-        next unless comment_node.loc.expression.source =~ /rubocop:disable/
+        next unless disabling?(comment_node)
 
-        add_offense(comment_node, :expression, OFFENSE)
+        add_offense(comment_node, :expression,
+                    [OFFENSE, *additional_comments(comment_node)].join("\n"))
       end
+    end
+
+    private
+
+    def disabling?(node)
+      node.loc.expression.source.include?(MAGIC_SUBSTRING)
+    end
+
+    def additional_comments(node)
+      node.loc.expression
+          .source
+          .scan(COP_PATTERN)
+          .flatten
+          .map { |name| SUGGESTIONS[name] }
+          .compact
     end
   end
 end
--- spec/ducalis/cops/rubocop_disable_spec.rb
+++ spec/ducalis/cops/rubocop_disable_spec.rb
@@ -8,6 +8,12 @@ require './lib/ducalis/cops/rubocop_disable'
 RSpec.describe Ducalis::RubocopDisable do
   subject(:cop) { described_class.new }
 
+  before do
+    stub_const('Ducalis::RubocopDisable::SUGGESTIONS',
+               'Metric/ParameterLists' => 'COMMENT_1',
+               'Metric/AbcSize' => 'COMMENT_2')
+  end
+
   it '[rule] raises on RuboCop disable comments' do
     inspect_source([
                      '# rubocop:disable Metrics/ParameterLists',
@@ -19,6 +25,15 @@ RSpec.describe Ducalis::RubocopDisable do
   it '[rule] better to follow RuboCop comments' do
     inspect_source('def calculate(five, context); end')
     expect(cop).not_to raise_violation
+  end
+
+  it 'could parse what cop was disabled' do
+    inspect_source([
+                     '# rubocop:disable Metric/AbcSize, Metric/ParameterLists',
+                     'def craZyMethoDName; end'
+                   ])
+    expect(cop).to raise_violation(/COMMENT_1/)
+    expect(cop).to raise_violation(/COMMENT_2/)
   end
 
   it 'ignores comment without RuboCop disabling' do
